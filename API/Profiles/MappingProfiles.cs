2d3ab0115c1d02e8484aa6f369f6ce24c206e8b8
IgmarLozadaBolivar <losadabolivar@gmail.com>
2023-10-20 22:04:59 -0500

    feat: :sparkles: Perfil de mapeo

--- API/Profiles/MappingProfiles.cs
+++ API/Profiles/MappingProfiles.cs
@@ -9,5 +9,28 @@ public class MappingProfiles : Profile
     {
         CreateMap<Rol, RolDto>().ReverseMap();
         CreateMap<User, UserDto>().ReverseMap();
+        CreateMap<Orden,OrdenDto>().ReverseMap();
+        CreateMap<Estado,EstadoDto>().ReverseMap();
+        CreateMap<Cargo,CargoDto>().ReverseMap();
+        CreateMap<Cliente,ClienteDto>().ReverseMap();
+        CreateMap<Color,ColorDto>().ReverseMap();
+        CreateMap<Departamento,DepartamentoDto>().ReverseMap();
+        CreateMap<DetalleOrden,DetalleOrdenDto>().ReverseMap();
+        CreateMap<DetalleVenta,DetalleVentaDto>().ReverseMap();
+        CreateMap<Empleado,EmpleadoDto>().ReverseMap();
+        CreateMap<Empresa,EmpresaDto>().ReverseMap();
+        CreateMap<FormaPago,FormaPagoDto>().ReverseMap();
+        CreateMap<Genero,GeneroDto>().ReverseMap();
+        CreateMap<Insumo,InsumoDto>().ReverseMap();
+        CreateMap<Inventario,InventarioDto>().ReverseMap();
+        CreateMap<Municipio,MunicipioDto>().ReverseMap();
+        CreateMap<Pais,PaisDto>().ReverseMap();
+        CreateMap<Prenda,PrendaDto>().ReverseMap();
+        CreateMap<Proveedor,ProveedorDto>().ReverseMap();
+        CreateMap<Talla,TallaDto>().ReverseMap();
+        CreateMap<TipoEstado,TipoEstadoDto>().ReverseMap();
+        CreateMap<TipoPersona,TipoPersonaDto>().ReverseMap();
+        CreateMap<TipoProteccion,TipoProteccionDto>().ReverseMap();
+        CreateMap<Venta,VentaDto>().ReverseMap();
     }
 }
